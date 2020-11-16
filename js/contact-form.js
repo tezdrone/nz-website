67d92a1cea96d29de234a31405ee8d1e0924ffa2
Aurorah Harmony <mineeraft@gmail.com>
2020-11-17 11:44:49 +1300

    Improve contact page

---
+++ js/contact-form.js
@@ -0,0 +1,30 @@
+var textarea = {
+	input: document.getElementById('msg'),
+	counter: document.getElementById('msgCounter'),
+};
+textarea.maxLength = textarea.input.dataset.maxLength;
+
+function updateCounter() {
+	var length = textarea.input.value.length,
+		max = textarea.maxLength;
+	textarea.counter.innerText = `${length}/${max}`;
+
+	if (length > max) {
+		return textarea.counter.classList.add('too-big');
+	}
+	textarea.counter.classList.remove('too-big');
+}
+
+updateCounter();
+
+textarea.input.addEventListener('input', () => {
+	updateCounter();
+});
+
+document.getElementById('contactForm').addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	if (textarea.input.value.length > textarea.maxLength) {
+		alert('Your message is too long');
+	}
+});
